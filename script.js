41f924405d4ab8e75d5e32662e2ffaf6991500c5
Tanner Nielsen <tan-ali5@outlook.com>
2020-04-24 22:28:20 -0700

    added questions to js

---
+++ script.js
@@ -0,0 +1,29 @@
+// Questions for quiz
+var questions = [
+    {
+        Q = "What characters are used to store/create an array?",
+        C = ["brackets, curly brackets, parenthses, double quotes"],
+        A = "brackets"
+    },
+    {
+        Q = "What characters are used to store/create a string?",
+        C = ["brackets, curly brackets, parenthses, double quotes"],
+        A = "double quotes"
+    },
+    {
+        Q = "How do we create random integers?",
+        C = ["Math.floor(Math.random() * #), Math.random(Math.floor() * #), Math.parse(Math.random() * #)"],
+        A = "Math.floor(Math.random() * #)"
+    },
+    {
+        Q = "What does i++ mean?",
+        C = ["index = index + 1, index = index + 2, index = number + 1, index"],
+        A = "index = index + 1"
+    },
+    {
+        Q = "What does JSON.Parse mean?",
+        C = ["object to string, string to object, object to array, string to array"]
+        A = "string to object"
+    },
+    
+];
